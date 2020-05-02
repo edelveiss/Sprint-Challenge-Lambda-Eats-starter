19839b64269189ec514e7f90ce07b7d00a34b180
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2020-05-01 18:29:38 -0700

    nav bar is created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route, Link, Switch, NavLink } from "react-router-dom";
+import shopsInfo from "./shops";
+import "./App.css";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  Button,
+} from "reactstrap";
 
 const App = () => {
+  const [collapsed, setCollapsed] = useState(true);
+  const toggleNavbar = () => setCollapsed(!collapsed);
+  const [shopInfo, setShopInfo] = useState(shopsInfo);
+  const [users, setUsers] = useState([]);
   return (
-    <>
+    <div className="App">
+      <div>
+        <Navbar color="warning" light>
+          <NavbarBrand
+            href="/"
+            className="mr-auto"
+            style={{
+              color: "red",
+              fontSize: "2.5rem",
+              fontWeight: "bolder",
+              paddingLeft: "3rem",
+            }}
+          >
+            Lambda Eats
+          </NavbarBrand>
+
+          <Button
+            onClick={toggleNavbar}
+            className="mr-2"
+            color="danger"
+            style={{
+              color: "white",
+            }}
+          >
+            Order
+          </Button>
+
+          <Collapse isOpen={!collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink
+                  onClick={toggleNavbar}
+                  to="/pizza"
+                  style={{
+                    color: "red",
+                    fontSize: "2rem",
+                    fontWeight: "bolder",
+                  }}
+                >
+                  Order Pizza
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  onClick={toggleNavbar}
+                  to="/"
+                  style={{
+                    color: "red",
+                    fontSize: "2rem",
+                    fontWeight: "bolder",
+                  }}
+                >
+                  Order Fresh Food
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
-    </>
+    </div>
   );
 };
 export default App;
---
+++ src/shops.js
@@ -0,0 +1,74 @@
+import mcdonald from "./assests/mcdonald.jpg";
+import roundtable from "./assests/roundtable.jpg";
+import starbucks from "./assests/starbucks.jpg";
+import subway from "./assests/subway.jpg";
+import sweetgreen from "./assests/sweetgreen.jpg";
+import tacobell from "./assests/tacobell.jpg";
+import chipotle from "./assests/chipotle.jpeg";
+
+const shopsInfo = [
+  {
+    id: 0,
+    shopName: "McDonald's",
+    shopDesc: "American - Fast Food - Burgers",
+    timeDelivery: "20-30 Min",
+    deliveryFee: 5.99,
+    image: mcdonald,
+    //image: m1,
+  },
+  {
+    id: 0,
+    shopName: "Chipotle",
+    shopDesc: "Mexican Grill",
+    timeDelivery: "30-40 Min",
+    deliveryFee: 4.99,
+    image: chipotle,
+    //image: m1,
+  },
+  {
+    id: 0,
+    shopName: "Starbucks",
+    shopDesc: "Cafe - Coffee&Tea",
+    timeDelivery: "20 Min",
+    deliveryFee: 3.99,
+    image: starbucks,
+    //image: m1,
+  },
+  {
+    id: 0,
+    shopName: "Subway",
+    shopDesc: "American - Fast Food - Burgers",
+    timeDelivery: "20-30 Min",
+    deliveryFee: 4.5,
+    image: subway,
+    //image: m1,
+  },
+  {
+    id: 0,
+    shopName: "Sweetgreen",
+    shopDesc: "Fresh - Healthy - Salads",
+    timeDelivery: "20-30 Min",
+    deliveryFee: 7,
+    image: sweetgreen,
+    //image: m1,
+  },
+  {
+    id: 0,
+    shopName: "Tacobell",
+    shopDesc: "American - Fast Food - Burgers",
+    timeDelivery: "20 Min",
+    deliveryFee: 3.99,
+    image: tacobell,
+    //image: m1,
+  },
+  //   {
+  //     id: 0,
+  //     shopName: "McDonald's",
+  //     shopDesc: "American - Fast Food - Burgers",
+  //     timeDelivery: "20-30 Min",
+  //     deliveryFee: 5.99,
+  //     image: mcdonald,
+  //     //image: m1,
+  //   },
+];
+export default shopsInfo;
